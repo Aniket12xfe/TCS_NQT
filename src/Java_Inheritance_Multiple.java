acec20f6af482921f5ff1411ab4e225985940453
Aniket Chaudhari <chaudharianiket379@gmail.com>
2024-06-16 15:25:42 +0530

    Initial commit

--- src/Java_Inheritance_Multiple.java
+++ src/Java_Inheritance_Multiple.java
@@ -6,7 +6,14 @@ interface Method2{
     public void method2();
 }
 
-class Main1 implements Method1, Method2{
+abstract class Method3{
+    abstract public void method3();
+    public void Method4(){
+        System.out.println("Method 4 execute...");
+    }
+}
+
+class Main1 extends Method3 implements Method1, Method2{
     @Override
     public void method1() {
         System.out.println("Method 1 execute...");
@@ -16,6 +23,11 @@ class Main1 implements Method1, Method2{
     public void method2() {
         System.out.println("Method 2 execute...");
     }
+
+    @Override
+    public void method3() {
+        System.out.println("Method 3 execute...");
+    }
 }
 
 public class Java_Inheritance_Multiple {
@@ -23,5 +35,7 @@ public class Java_Inheritance_Multiple {
         Main1 main1 = new Main1();
         main1.method1();
         main1.method2();
+        main1.method3();
+        main1.Method4();
     }
 }
